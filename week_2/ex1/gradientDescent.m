d3c793593852ac9e06fcf6b39abdf480eec607a9
Preston O'Neal <prestononeal@gmail.com>
2017-11-11 14:05:36 -0800

    Add single variable gradient descent

--- week_2/ex1/gradientDescent.m
+++ week_2/ex1/gradientDescent.m
@@ -16,12 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    
+    % Compute the difference matrix between h(x) and y
+    temp0 = theta(1,1) - alpha * sum(X*theta - y)/m;
+    temp1 = theta(2,1) - alpha * sum((X*theta - y).*X(:,2))/m;
 
-
-
-
-
-
+    theta(1,1) = temp0;
+    theta(2,1) = temp1;
 
     % ============================================================
 
