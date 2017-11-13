f61999d280233378a7d1d20c36173b19bd4f4292
Preston O'Neal <prestononeal@gmail.com>
2017-11-12 20:12:11 -0800

    Add sigmoid function

--- week_3/ex2/sigmoid.m
+++ week_3/ex2/sigmoid.m
@@ -8,11 +8,7 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
-
+g = arrayfun(@(x) 1/(1 + exp(-x)), z);
 % =============================================================
 
 end
