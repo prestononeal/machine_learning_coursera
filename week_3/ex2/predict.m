0323ff1176908dfb26df8da2b938bbcd969f6fa1
Preston O'Neal <prestononeal@gmail.com>
2017-11-12 21:14:26 -0800

    Add prediction

--- week_3/ex2/predict.m
+++ week_3/ex2/predict.m
@@ -14,13 +14,7 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
-
-
+p = arrayfun(@(x) x >= 0.5, sigmoid(X*theta));
 % =========================================================================
 
 
