f812fa480e43dda87a1cf41033e2f757f919917e
Preston O'Neal <prestononeal@gmail.com>
2017-11-12 19:57:18 -0800

    Add data plotting to exam scores

--- week_3/ex2/plotData.m
+++ week_3/ex2/plotData.m
@@ -12,14 +12,11 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+% Find positive and negative values so they can be plotted separately
+pos = find(y == 1); neg = find(y == 0);
 
-
-
-
-
-
-
-
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 % =========================================================================
 
 
