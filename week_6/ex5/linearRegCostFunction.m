7cec8d1861109e5b1ef3ba19250fb44553ffc7eb
Preston O'Neal <prestononeal@gmail.com>
2017-12-10 18:36:58 -0800

    Add regularized linear regression cost function

--- week_6/ex5/linearRegCostFunction.m
+++ week_6/ex5/linearRegCostFunction.m
@@ -18,18 +18,20 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+% Calculate the cost function
+J = sum((X*theta - y) .^ 2);
 
+% Calculate the gradient values
+grad = X'*(X*theta - y);
 
+% Add the regularization terms
+theta(1, 1) = 0;  % Don't regularize the bias term
+J = J + lambda*sum(theta .^ 2);
+grad = grad + theta*lambda;
 
-
-
-
-
-
-
-
-
-
+% Scale by m
+J = J/(2*m);
+grad = grad/m;
 % =========================================================================
 
 grad = grad(:);
