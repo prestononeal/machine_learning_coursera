759c8ef5788ad7fa6c5d21dec8c863d21bd3f4cf
Preston O'Neal <prestononeal@gmail.com>
2017-12-10 20:06:12 -0800

    Add learning curves

--- week_6/ex5/learningCurve.m
+++ week_6/ex5/learningCurve.m
@@ -50,17 +50,15 @@ error_val   = zeros(m, 1);
 %           
 %       end
 %
-
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
+for i = 1:m
+    theta = trainLinearReg(X(1:i, :), y(1:i, :), lambda);
+    [J_train, ~] = linearRegCostFunction( ...
+        X(1:i, :), y(1:i, :), theta, 0);
+    [J_cross, ~] = linearRegCostFunction( ...
+        Xval, yval, theta, 0);
+    error_train(i) = J_train;
+    error_val(i) = J_cross;
+end
 % =========================================================================
 
 end
