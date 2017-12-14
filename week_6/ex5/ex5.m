df13c6f414d2e24137d2f2273d2f8d2d721f33ed
Preston O'Neal <prestononeal@gmail.com>
2017-12-14 00:06:10 -0800

    Select lambda from cross validation set

--- week_6/ex5/ex5.m
+++ week_6/ex5/ex5.m
@@ -164,7 +164,7 @@ pause;
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 0;
+lambda = 1;  % Lambda = 0 is too low and overfits. Lambda = 100 underfits 
 [theta] = trainLinearReg(X_poly, y, lambda);
 
 % Plot training data and fit
