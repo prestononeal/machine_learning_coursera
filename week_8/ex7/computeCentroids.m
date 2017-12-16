ae4698e1b80184e3166f64a8eabe78a059618df8
Preston O'Neal <prestononeal@gmail.com>
2017-12-16 15:02:49 -0800

    Compute centroid means

--- week_8/ex7/computeCentroids.m
+++ week_8/ex7/computeCentroids.m
@@ -25,14 +25,11 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
-
+% Loop over each centroid
+for i=1:K
+    sel = find(idx == i);
+    centroids(i,:) = mean(X(sel,:));
+end
 % =============================================================
 
 
